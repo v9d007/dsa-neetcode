4c2fe512adfddaaa52b57e69bb9395a53f1a55e7
Vinod Kumar Singh <130828719+v9d007@users.noreply.github.com>
2026-06-17 11:49:32 +0530

    Add: counting-bits - submission-0

---
+++ Data Structures & Algorithms/counting-bits/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number[]}
+     */
+    countBits(n) {
+        let result = []
+
+        for(let i=0; i<=n; i++){
+            let count = 0
+            let current = i
+
+            while(current != 0){
+                current = current & (current-1)
+                count++
+            }
+
+            result.push(count)
+        }
+
+        return result
+    }
+}
